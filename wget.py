42ce11b2b2ddb1d3df47d48555f3de24c8f3ba54
thisarray <59105532+thisarray@users.noreply.github.com>
2020-02-09 20:14:28 -0800

    Added a script to get an URL.

---
+++ wget.py
@@ -0,0 +1,23 @@
+"""Python 3 script to get an URL."""
+
+if __name__ == '__main__':
+    import argparse
+    import os.path
+    import urllib.request
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-o', '--output', default='',
+                        help='write the response to the file at path')
+    parser.add_argument('url',
+                        help='URL to get')
+    args = parser.parse_args()
+
+    if os.path.isfile(args.output):
+        parser.exit('{0} already exists!'.format(args.output))
+
+    request = urllib.request.Request(args.url)
+    with urllib.request.urlopen(request) as response:
+        if len(args.output) <= 0:
+            print(response.read().decode('utf-8'))
+        else:
+            with open(args.output, 'wb') as f:
+                f.write(response.read())
