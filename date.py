4c1ff59adc110f0a2fb4214dcd7a0cb95ccff126
thisarray <59105532+thisarray@users.noreply.github.com>
2020-02-14 21:54:01 -0800

    Used fixed dates in the tests to identify bugs in date.py.

--- date.py
+++ date.py
@@ -7,7 +7,7 @@ WHITE_LIST = 'AaBbcdfGHIjMmpSUuVWwXxYyZz%'
 """List of format codes Python understands."""
 
 NEW_CODE_MAP = {
-    'C': lambda dt: '{0:02d}'.format(dt.year % 100),
+    'C': lambda dt: '{0:04d}'.format(dt.year)[:2],
     'D': lambda dt: '%m/%d/%y',
     'e': lambda dt: ' %d',
     'F': lambda dt: '%Y-%m-%d',
@@ -17,9 +17,10 @@ NEW_CODE_MAP = {
     'l': lambda dt: ' %I',
     'N': lambda dt: '{0:09d}'.format(dt.microsecond * 1000),
     'n': lambda dt: '\n',
+    'q': lambda dt: '{0:d}'.format(((dt.month - 1) // 3) + 1),
     'P': lambda dt: dt.strftime('%p').lower(),
     'R': lambda dt: '%H:%M',
-    'r': lambda dt: '%I:%M',
+    'r': lambda dt: '%I:%M:%S %p',
     's': lambda dt: str(dt.timestamp()),
     'T': lambda dt: '%H:%M:%S',
     't': lambda dt: '\t'
@@ -86,6 +87,7 @@ class _UnitTest(unittest.TestCase):
             self.assertEqual(format_datetime(utc_now, '%%' + value),
                              '%' + value)
         for value, expected in [
+            ('', ''),
             ('%%', '%'),
             ('%Y', '{0:04d}'.format(utc_now.year)),
             ('%m', '{0:02d}'.format(utc_now.month)),
@@ -100,7 +102,7 @@ class _UnitTest(unittest.TestCase):
             ('%l', utc_now.strftime(' %I')),
             ('%n', '\n'),
             ('%R', utc_now.strftime('%H:%M')),
-            ('%r', utc_now.strftime('%I:%M')),
+            ('%r', utc_now.strftime('%I:%M:%S %p')),
             ('%T', utc_now.strftime('%H:%M:%S')),
             ('%t', '\t')]:
             self.assertEqual(format_datetime(utc_now, value), expected)
@@ -117,11 +119,140 @@ class _UnitTest(unittest.TestCase):
         else:
             self.assertEqual(format_datetime(utc_now, '%P'), 'pm')
             self.assertEqual(format_datetime(utc_now, '%p'), 'PM')
-        utc_now = utc_now.replace(microsecond=0, tzinfo=None)
-        self.assertEqual(format_datetime(utc_now, '%Y-%m-%dT%H:%M:%S'),
-                         utc_now.isoformat())
-        self.assertEqual(format_datetime(utc_now, '%Y-%m-%d %H:%M:%S'),
-                         utc_now.isoformat(sep=' '))
+        for value in range(1, 13):
+            utc_now = utc_now.replace(month=value)
+            if value < 4:
+                expected = '1'
+            elif value < 7:
+                expected = '2'
+            elif value < 10:
+                expected = '3'
+            else:
+                expected = '4'
+            self.assertEqual(format_datetime(utc_now, '%q'), expected)
+            utc_now = utc_now.replace(microsecond=0, tzinfo=None)
+            self.assertEqual(format_datetime(utc_now, '%Y-%m-%dT%H:%M:%S'),
+                             utc_now.isoformat())
+            self.assertEqual(format_datetime(utc_now, '%Y-%m-%d %H:%M:%S'),
+                             utc_now.isoformat(sep=' '))
+
+        dt = datetime.datetime(2001, 2, 3, 4, 5, 6, 7)
+        for value, expected in [
+            ('', ''),
+            ('%%', '%'),
+            ('%A', 'Saturday'),
+            ('%a', 'Sat'),
+            ('%B', 'February'),
+            ('%b', 'Feb'),
+            ('%C', '20'),
+            ('%c', 'Sat Feb  3 04:05:06 2001'),
+            ('%D', '02/03/01'),
+            ('%d', '03'),
+            ('%e', ' 03'),
+            ('%F', '2001-02-03'),
+            ('%G', '2001'),
+            ('%g', '01'),
+            ('%H', '04'),
+            ('%h', 'Feb'),
+            ('%I', '04'),
+            ('%j', '034'),
+            ('%k', ' 04'),
+            ('%l', ' 04'),
+            ('%M', '05'),
+            ('%m', '02'),
+            ('%N', '000007000'),
+            ('%n', '\n'),
+            ('%P', 'am'),
+            ('%p', 'AM'),
+            ('%q', '1'),
+            ('%R', '04:05'),
+            ('%r', '04:05:06 AM'),
+            ('%S', '06'),
+            ('%s', '981201906.000007'),
+            ('%T', '04:05:06'),
+            ('%t', '\t'),
+            ('%U', '04'),
+            ('%u', '6'),
+            ('%V', '05'),
+            ('%W', '05'),
+            ('%w', '6'),
+            ('%X', '04:05:06'),
+            ('%x', '02/03/01'),
+            ('%Y', '2001'),
+            ('%y', '01'),
+            ('Today is %A, %B %d, %Y',
+             'Today is Saturday, February 03, 2001'),
+            ('Today is %A, %B%e, %Y',
+             'Today is Saturday, February 03, 2001'),
+            ('The current time is %H:%M:%S %p',
+             'The current time is 04:05:06 AM')]:
+            self.assertEqual(format_datetime(dt, value), expected)
+            for c in WHITE_LIST[:-1]:
+                self.assertEqual(format_datetime(dt, c + value),
+                                 c + expected)
+                self.assertEqual(format_datetime(dt, value + c),
+                                 expected + c)
+                self.assertEqual(format_datetime(dt, c + value + c),
+                                 c + expected + c)
+
+        dt = datetime.datetime(2011, 12, 13, 14, 15, 16, 17)
+        for value, expected in [
+            ('', ''),
+            ('%%', '%'),
+            ('%A', 'Tuesday'),
+            ('%a', 'Tue'),
+            ('%B', 'December'),
+            ('%b', 'Dec'),
+            ('%C', '20'),
+            ('%c', 'Tue Dec 13 14:15:16 2011'),
+            ('%D', '12/13/11'),
+            ('%d', '13'),
+            ('%e', ' 13'),
+            ('%F', '2011-12-13'),
+            ('%G', '2011'),
+            ('%g', '11'),
+            ('%H', '14'),
+            ('%h', 'Dec'),
+            ('%I', '02'),
+            ('%j', '347'),
+            ('%k', ' 14'),
+            ('%l', ' 02'),
+            ('%M', '15'),
+            ('%m', '12'),
+            ('%N', '000017000'),
+            ('%n', '\n'),
+            ('%P', 'pm'),
+            ('%p', 'PM'),
+            ('%q', '4'),
+            ('%R', '14:15'),
+            ('%r', '02:15:16 PM'),
+            ('%S', '16'),
+            ('%s', '1323814516.000017'),
+            ('%T', '14:15:16'),
+            ('%t', '\t'),
+            ('%U', '50'),
+            ('%u', '2'),
+            ('%V', '50'),
+            ('%W', '50'),
+            ('%w', '2'),
+            ('%X', '14:15:16'),
+            ('%x', '12/13/11'),
+            ('%Y', '2011'),
+            ('%y', '11'),
+            ('Today is %A, %B %d, %Y',
+             'Today is Tuesday, December 13, 2011'),
+            ('Today is %A, %B%e, %Y',
+             'Today is Tuesday, December 13, 2011'),
+            ('The current time is %H:%M:%S %p',
+             'The current time is 14:15:16 PM')]:
+            self.assertEqual(format_datetime(dt, value), expected)
+            for c in WHITE_LIST[:-1]:
+                self.assertEqual(format_datetime(dt, c + value),
+                                 c + expected)
+                self.assertEqual(format_datetime(dt, value + c),
+                                 expected + c)
+                self.assertEqual(format_datetime(dt, c + value + c),
+                                 c + expected + c)
 
 if __name__ == '__main__':
     import argparse
