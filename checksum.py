7305f5e1f59c504745eb02bf1c2a1c65916df75e
thisarray <59105532+thisarray@users.noreply.github.com>
2020-12-03 17:50:28 -0800

    Added a script to establish one directory is a subset of another directory.

--- checksum.py
+++ checksum.py
@@ -18,8 +18,8 @@ def checksum(path, name=_DEFAULT_HASH, size=_SIZE):
         path: String path to the file.
         name: Optional string name of the hash algorithm in hashlib to use.
             Defaults to _DEFAULT_HASH.
-        size: Optional positive integer number of bytes to read from the file
-            at a time. Defaults to _SIZE.
+        size: Optional positive int number of bytes to read from the file at
+            a time. Defaults to _SIZE.
     Returns:
         String hexadecimal digest checksum of the file at path.
     """
@@ -27,11 +27,14 @@ def checksum(path, name=_DEFAULT_HASH, size=_SIZE):
         raise TypeError('path must be a valid string path to a file.')
     if not os.path.isfile(path):
         raise ValueError('path must be a valid string path to a file.')
+    if not isinstance(name, str):
+        raise TypeError('name must be a string.')
     if not isinstance(size, int):
-        raise TypeError('size must be a positive integer > 1024.')
+        raise TypeError('size must be a positive int > 1024.')
     if size <= 1024:
-        raise ValueError('size must be a positive integer > 1024.')
+        raise ValueError('size must be a positive int > 1024.')
 
+    name = name.strip().lower()
     m = hashlib.new(name)
     with open(path, 'rb') as f:
         while True:
@@ -47,12 +50,10 @@ def checksum(path, name=_DEFAULT_HASH, size=_SIZE):
 class _UnitTest(unittest.TestCase):
     def test_checksum(self):
         """Test calculating the checksum for a file."""
-        for value in [None, 42, []]:
+        for value in [None, 42.0, []]:
             self.assertRaises(TypeError, checksum, value)
             self.assertRaises(TypeError, checksum, 'checksum.py', value)
-            if not isinstance(value, int):
-                self.assertRaises(TypeError, checksum, 'checksum.py',
-                                  size=value)
+            self.assertRaises(TypeError, checksum, 'checksum.py', size=value)
         for value in ['', 'foobar']:
             self.assertRaises(ValueError, checksum, value)
             self.assertRaises(ValueError, checksum, 'checksum.py', value)
@@ -80,4 +81,5 @@ if __name__ == '__main__':
                 paths.append(path)
         paths.sort()
         for path in paths:
-            print('{0}  {1}'.format(checksum(path, hash_name), path))
+            print('{0}  {1}'.format(checksum(path, hash_name),
+                                    os.path.basename(path)))
